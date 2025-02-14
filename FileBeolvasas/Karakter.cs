8ba3ea4a92c15f021dcfb032f146e109572c841e
Petrik Diak <diak@petrik.hu>
2025-02-14 12:07:11 +0100

    az első 5 feladat kész, félidős commit a biztonság kedvéért. :)

---
+++ FileBeolvasas/Karakter.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileBeolvasas
+{
+	internal class Karakter
+	{
+		private string name;
+		private int ero;
+		private int eletero;
+		private int szint;
+
+		public Karakter(string name, int ero, int eletero, int szint)
+		{
+			this.name = name;
+			this.ero = ero;
+			this.eletero = eletero;
+			this.szint = szint;
+		}
+
+		public string Name { get => name; set => name = value; }
+		public int Ero { get => ero; set => ero = value; }
+		public int Eletero { get => eletero; set => eletero = value; }
+		public int Szint { get => szint; set => szint = value; }
+
+
+		
+
+
+
+
+
+
+
+
+
+
+
+		public override string? ToString()
+		{
+			return $"Name: {name}, Ero: {ero}, Eletero: {eletero}, Szint: {szint}";
+		}
+	}
+}
--- FileBeolvasas/Program.cs
+++ FileBeolvasas/Program.cs
@@ -1,10 +1,119 @@
-﻿namespace FileBeolvasas
+﻿using System.Diagnostics.Tracing;
+
+namespace FileBeolvasas
 {
 	internal class Program
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello, World!");
+			List<Karakter> karakterek = [];
+			Beolvasas("karakterek.txt", karakterek);
+			MaximumElet(karakterek);
+			AtlagSzint(karakterek);
+			RendezoEro(karakterek);
+			NagyobbE50Nel(karakterek);
+
+
 		}
+
+		static void Beolvasas(string filenev, List<Karakter> karakterek)
+		{
+			StreamReader sr = new(filenev);
+			string[] sorok = File.ReadAllLines(filenev);
+
+
+			sr.ReadLine();
+
+			while (!sr.EndOfStream)
+			{
+				string sor = sr.ReadLine();
+				string[] szavak = sor.Split(';');
+
+
+				Karakter karakter = new(szavak[0], Convert.ToInt16(szavak[1]), Convert.ToInt16(szavak[2]), Convert.ToInt16(szavak[3]));
+				karakterek.Add(karakter);
+			}
+
+		}
+
+
+
+		static void MaximumElet(List<Karakter> karakterek)
+		{
+			int max = karakterek[0].Eletero;
+			string name = karakterek[0].Name;
+			foreach (var item in karakterek)
+			{
+				if (item.Eletero > max)
+				{
+					max = item.Eletero;
+				}
+			}
+
+			Console.WriteLine("");
+			Console.WriteLine("2. Feladat");
+			Console.WriteLine($"{name}-nek, {max} életereje van.");
+		}
+
+		static void AtlagSzint(List<Karakter> karakterek)
+		{
+			int osszeg = 0;
+			foreach (var item in karakterek)
+			{
+				osszeg += item.Szint;
+			}
+			double atlag = osszeg / karakterek.Count;
+			Console.WriteLine("");
+			Console.WriteLine("3. Feladat");
+			Console.WriteLine($"A karakterek átlag szintje: {atlag}");
+		}
+
+		static void RendezoEro(List<Karakter> karakterek)
+		{
+			karakterek.Sort((x, y) => x.Ero.CompareTo(y.Ero));
+			Console.WriteLine("");
+			Console.WriteLine("4. Feladat");
+			Console.WriteLine("A karakterek erő szerinti rendezése:");
+            Console.WriteLine("");
+            foreach (var item in karakterek)
+			{
+				Console.WriteLine(item);
+			}
+		}
+
+		static void NagyobbE50Nel(List<Karakter> karakterek)
+		{
+			Console.WriteLine("");
+			Console.WriteLine("5. Feladat");
+			Console.WriteLine("Azok a karakterek, akiknek az ereje nagyobb, mint 50:");
+			foreach (var item in karakterek)
+			{
+				if (item.Ero > 50)
+				{
+					Console.WriteLine(item);
+				}
+			}
+		}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 	}
+
+
+
+
 }
+
+
