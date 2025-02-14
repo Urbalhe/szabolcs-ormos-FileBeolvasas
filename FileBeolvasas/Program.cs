aa25509a06eea119098ccd088dabe41943a633e6
Petrik Diak <diak@petrik.hu>
2025-02-14 12:33:16 +0100

    Kész :), órai munka.

--- FileBeolvasas/Program.cs
+++ FileBeolvasas/Program.cs
@@ -12,6 +12,11 @@ namespace FileBeolvasas
 			AtlagSzint(karakterek);
 			RendezoEro(karakterek);
 			NagyobbE50Nel(karakterek);
+			KarakterStats(karakterek);
+			//CSVBeir(karakterek);
+			LegjobbHarom(karakterek);
+			KarakterRangsor(karakterek);
+			CsataVerseny(karakterek);
 
 
 		}
@@ -74,8 +79,8 @@ namespace FileBeolvasas
 			Console.WriteLine("");
 			Console.WriteLine("4. Feladat");
 			Console.WriteLine("A karakterek erő szerinti rendezése:");
-            Console.WriteLine("");
-            foreach (var item in karakterek)
+			Console.WriteLine("");
+			foreach (var item in karakterek)
 			{
 				Console.WriteLine(item);
 			}
@@ -97,23 +102,90 @@ namespace FileBeolvasas
 
 
 
+		static void KarakterStats(List<Karakter> karakterek)
+		{
+			Console.WriteLine("");
+			Console.WriteLine("6. Feladat");
+			Console.WriteLine("Karakterek statisztikája:");
+			Console.WriteLine("Név\tEro\tÉleterő\tSzint");
+			foreach (var item in karakterek)
+			{
+				Console.WriteLine($"{item.Name}\t{item.Ero}\t{item.Eletero}\t{item.Szint}");
+			}
+		}
+
+		//static void CSVBeir(List<Karakter> karakterek)
+		//{
+		//          Console.WriteLine("");
+		//          Console.WriteLine("7. Feladat");
+		//          StreamWriter sw = new("karakterek_rendezett.csv");
+		//	sw.WriteLine("Név;Ero;Életerő;Szint");
+		//	foreach (var item in karakterek)
+		//	{
+		//		sw.WriteLine($"{item.Name};{item.Ero};{item.Eletero};{item.Szint}");
+		//	}
+		//	sw.Close();
+		//}
+
+
+		static void LegjobbHarom(List<Karakter> karakterek)
+		{
+			Console.WriteLine("");
+			Console.WriteLine("8. Feladat");
+			Console.WriteLine("A legjobb 3 karakter:");
+			karakterek.Sort((x, y) => y.Ero.CompareTo(x.Ero));
+			for (int i = 0; i < 3; i++)
+			{
+				Console.WriteLine(karakterek[i]);
+			}
+
+		}
+
+
+		static void KarakterRangsor(List<Karakter> karakterek)
+		{
+			Console.WriteLine("");
+			Console.WriteLine("9. Feladat");
+			Console.WriteLine("Karakterek rangsora:");
+			Console.WriteLine("Név\tEro\tÉleterő\tSzint");
+			int helyezes = 1;
+			foreach (var item in karakterek)
+			{
+				Console.WriteLine($"{helyezes}. {item.Name}\t{item.Ero}\t{item.Eletero}\t{item.Szint}");
+				helyezes++;
+			}
+		}
 
 
 
 
+		static void CsataVerseny(List<Karakter> karakterek)
+		{
+			Console.WriteLine("");
+			Console.WriteLine("10. Feladat");
+			Console.WriteLine("Csata verseny:");
+			Random rnd = new();
+			int karakter1 = rnd.Next(0, karakterek.Count);
+			int karakter2 = rnd.Next(0, karakterek.Count);
+			Console.WriteLine($"{karakterek[karakter1].Name} vs {karakterek[karakter2].Name}");
+			if (karakterek[karakter1].Ero > karakterek[karakter2].Ero)
+			{
+				Console.WriteLine($"{karakterek[karakter1].Name} nyert!");
+			}
+			else if (karakterek[karakter1].Ero < karakterek[karakter2].Ero)
+			{
+				Console.WriteLine($"{karakterek[karakter2].Name} nyert!");
+			}
+			else
+			{
+				Console.WriteLine("Döntetlen!");
+			}
 
 
 
 
-
-
-
-
+		}
 	}
-
-
-
-
 }
 
 
